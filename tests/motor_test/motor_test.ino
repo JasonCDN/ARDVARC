02ffaf141166e8c88baed3e27ade5fe4de57a61c
Jason CDN <jasoncdn@users.noreply.github.com>
2017-05-26 17:38:53 +1000

    Simplified motor test

--- tests/motor_test/motor_test.ino
+++ tests/motor_test/motor_test.ino
@@ -8,31 +8,17 @@ ArmControl arm;
 void setup() {
   Serial.begin(9600);
   arm.setServoPins(A0, A1, A2);
-  driver.setSpeed(0.9); // Preserve motors
+  driver.setSpeed(1); // Preserve motors
   driver.setMotorPins(3, 4, 2, 5, 6, 7);
   driver.setWheelDiameter(55);
   driver.setTrackWidth(105);
-  driver.setRevsPerDC(22.5);
+  driver.setRevsPerDC(12);
   driver.setBackScaling(1);
 }
 
 void loop() {
   arm.restPosition(); // Required, to keep servos steady
-  driver.forward(12000);
-  driver.pause(500);
-  driver.backward(50);
-  driver.pause(500);
-  driver.turnRight(45);
-  driver.pause(500);
-  driver.turnLeft(90);
-  driver.pause(500);
-  driver.turnRight(45);
-  driver.pause(500);
-  driver.nudge(20, 20);
-  driver.pause(500);
-  driver.goToPoint(50, 100);
-  driver.pause(500);
-  driver.goToPointSticky(-50, -100);
+  driver.forward(1000);
 
   while (true) {
     driver.run();
