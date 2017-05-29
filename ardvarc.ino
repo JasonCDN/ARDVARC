d5aec0be425af44cb4d02ba01056391bd298b0f5
Jason CDN <jasoncdn@users.noreply.github.com>
2017-05-29 16:51:54 +1000

    Updated main pins list

--- ardvarc.ino
+++ ardvarc.ino
@@ -8,7 +8,19 @@ SensorControl sensors;
 
 
 void setup() {
-	Serial.begin(9600);
+  Serial.begin(9600);
+  // Arm
+  arm.setServoPins(A1, A2, A3);
+  // Sensors
+  sensors.setSensorPins(10, 11, 8, 9, 12);
+  // Driver (motors)
+  driver.setSpeed(1);
+  driver.setMotorPins(3, 4, 2, 5, 6, 7);
+  driver.setWheelDiameter(55);
+  driver.setTrackWidth(105);
+  driver.setRevsPerDC(11);
+  driver.setWheelScales(2, 1);
+  driver.setBackScaling(0.1);
 }
 
 void loop() {
